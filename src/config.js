533ebf20597301a83fbd6ec31d1de1850f570dd9
Timothy Kim <101423302+Ssipbunhal@users.noreply.github.com>
2025-03-21 05:01:05 -0600

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 
 const CONFIG = {
-    API_BASE_URL: process.env.REACT_APP_API_BASE_URL
+    API_BASE_URL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:5001/api' // fallback for tests
   };
   
   export default CONFIG;
