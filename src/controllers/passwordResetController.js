7105345cc82cc275edc260ea5723151977556471
Timothy Kim <101423302+Ssipbunhal@users.noreply.github.com>
2025-03-21 05:10:44 -0600

    Update passwordResetController.js

--- src/controllers/passwordResetController.js
+++ src/controllers/passwordResetController.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import CONFIG from "../config";
+import CONFIG from "../config.js";
 // TODO: Send api call to backend with email to create reset token
 export const sendResetLink = async (email) => {
     try {
